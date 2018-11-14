885657c80aacefa7b7b1f75063ce75b7447ecd56
Michael Lauritsen <i30288741@live.dk>
2018-11-14 08:26:59 +0100

    Example of Threading

---
+++ threadingdemo/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace threadingdemo
+{
+    class ThreadTest
+    {
+        bool done;
+
+        static void main()
+        {
+            ThreadTest tt = new ThreadTest();           // Create a common instance
+            new Thread(tt.Go).Start();
+            tt.Go();
+
+        }
+
+        void Go()
+        {
+            if (!done)
+            {
+                done = true;
+                Console.WriteLine("Done");
+            }
+        }
+    }
+}
--- threadingdemo/Program.cs
+++ threadingdemo/Program.cs
@@ -2,14 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace threadingdemo
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            Thread t = new Thread(WriteY);  // start a new thread
+            t.Start();                      // Run WriteY()
+
+            new Thread(Go).Start();         // Start a new Thread
+            Go();                           // Call Go() on the main thread
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("x");
+            }
         }
+
+        static void Go()
+        {
+            for (int cycles = 0; cycles < 5; cycles++)
+            {
+                // Decalre and use a Local variable - 'cycles'
+                Console.Write("?");
+            }
+        }
+
+        static void WriteY()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("y");
+            }
+
+            Console.ReadKey();
+        }
+
+        
+        
     }
 }
